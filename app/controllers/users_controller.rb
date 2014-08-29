bec6350cc16c0ebee009d41809b40737bbc596e3
Chris Callahan <callahan.c@gmail.com>
2014-08-29 16:07:33 -0400

    finds and appends 5 closest doctors to page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def index
     @user = User.new
-    @doctors = Doctor.all
   end
 
   def create
+    coordinates = Geocoder.coordinates(user_address)
+    closest_doctors = Doctor.all.sort_by do |doctor|
+      Geocoder::Calculations.distance_between(coordinates, [doctor.latitude, doctor.longitude])
+    end
+
+    @five_closest_doctors = closest_doctors[0..4]
   end
+
+  private
+    def user_address
+      [params[:user][:address], params[:user][:city], params[:user][:state]].join(', ')
+    end
 end
